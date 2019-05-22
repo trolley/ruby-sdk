1dd839e590cc844ee431c0d72671112da41c7ba4
Josh Cunningham <jbc.okay@gmail.com>
2019-05-22 15:50:21 -0400

    PaymentRails to act as client

--- lib/paymentrails.rb
+++ lib/paymentrails.rb
@@ -1,11 +1,33 @@
 class PaymentRails
-  def Gateway
-    load 'Gateway.rb'
-    return Gateway
-  end
-  
-  def Configuration
+  attr_reader :recipient
+  attr_writer :recipient
+
+  attr_reader :recipient_account
+  attr_writer :recipient_account
+
+  attr_reader :batch
+  attr_writer :batch
+
+  attr_reader :payment
+  attr_writer :payment
+
+  attr_reader :balance
+  attr_writer :balance
+
+  def initialize(key, secret, environment = 'production')
     load 'Configuration.rb'
-    return Configuration
+    load 'Gateway.rb'
+
+    client = Gateway.new(Configuration.new(key, secret, environment))
+    @recipient = client.recipient
+    @recipient_account = client.recipient_account
+    @batch = client.batch
+    @payment = client.payment
+    @balance = client.balance
+
+    # to avoid overriding potential global Configuration & Gateway
+    Object.send(:remove_const, :Gateway)
+    Object.send(:remove_const, :Configuration)
   end
+
 end
