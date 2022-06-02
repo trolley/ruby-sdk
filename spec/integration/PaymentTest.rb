56257647faee7e7c08c46da00d57e2db63d09802
zero-bytes <zero.bites.inbox@gmail.com>
2022-06-02 08:51:10 -0600

    payment need to be processable

--- spec/integration/PaymentTest.rb
+++ spec/integration/PaymentTest.rb
@@ -4,6 +4,15 @@ class PaymentTest < Test::Unit::TestCase
   DEFAULT_PAYMENT_AMOUNT = "100.00"
   DEFAULT_PAYMENT_CURRENCY = 'USD'
 
+  DEFAULT_ACCOUNT_TYPE = 'bank-transfer'
+  DEFAULT_ACCOUNT_CURRENCY = 'USD'
+  DEFAULT_ACCOUNT_COUNTRY = 'US'
+  DEFAULT_ACCOUNT_HOLDER_NAME = 'John Doe'
+  DEFAULT_ACCOUNT_NUMBER = '1234567890'
+  DEFAULT_ACCOUNT_NUMBER_REDACTED = '********90'
+  DEFAULT_ACCOUNT_SWIFT = 'USBKUS44MCT'
+  DEFAULT_ACCOUNT_BRANCH_ID = '091000022'
+
   def setup
     @client = PaymentRails.client(
       ENV.fetch('SANDBOX_API_KEY'),
@@ -24,16 +33,38 @@ class PaymentTest < Test::Unit::TestCase
 
   def create_payment
     uuid = SecureRandom.uuid.to_s
-    batch = @client.batch.create({})
+    batch = @client.batch.create(
+      sourceCurrency: 'USD', description: 'Integration Test Payments'
+    )
 
     recipient = @client.recipient.create(
       firstName: 'John',
       lastName: 'Doe',
       email: "john.doe+#{uuid}@example.com",
       type: 'individual',
+      address: {
+        street1: '123 NYC',
+        city: 'NY',
+        country: 'US',
+        postalCode: '99950'
+      }
       )
 
-    return @client.payment.create(
+    @client.recipient_account.create(
+      recipient.id,
+      {
+        type: DEFAULT_ACCOUNT_TYPE,
+        currency: DEFAULT_ACCOUNT_CURRENCY,
+        country: DEFAULT_ACCOUNT_COUNTRY,
+        accountHolderName: DEFAULT_ACCOUNT_HOLDER_NAME,
+        accountNumber: DEFAULT_ACCOUNT_NUMBER,
+        accountNum: DEFAULT_ACCOUNT_NUMBER,
+        swiftBic: DEFAULT_ACCOUNT_SWIFT,
+        branchId: DEFAULT_ACCOUNT_BRANCH_ID
+      }
+    )
+
+    payment = @client.payment.create(
       batch.id,
       recipient: {
         id: recipient.id,
@@ -41,5 +72,9 @@ class PaymentTest < Test::Unit::TestCase
       amount: DEFAULT_PAYMENT_AMOUNT,
       currency: DEFAULT_PAYMENT_CURRENCY
     )
+
+    @client.batch.start_processing(batch.id)
+
+    payment
   end
 end
