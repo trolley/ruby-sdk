6b1e42acdbc890b47bea03f26674b68743ad01d3
Zero <zero.bites.inbox@gmail.com>
2023-07-17 21:06:48 -0600

    cover balance

---
+++ spec/integration/BalanceTest.rb
@@ -0,0 +1,18 @@
+require_relative 'helper'
+
+class BalanceTest < Test::Unit::TestCase
+  include ApiClientHelper
+
+  def test_find
+    balances = @client.balance.find
+    assert_true(balances.balances.count > 0)
+    assert_not_nil(balances.balances[0].type)
+  end
+
+  def test_find_with_term
+    balances = @client.balance.find('paymentrails')
+    assert_true(balances.balances.count > 0)
+    assert_not_nil(balances.balances[0].type)
+    assert_equal('paymentrails', balances.balances[0].type)
+  end
+end
