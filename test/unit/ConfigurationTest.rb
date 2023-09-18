379982454d294a95fa49e1d592b66a9577ac30ea
Zero <zero.bites.inbox@gmail.com>
2023-09-18 09:00:32 -0600

    fixup name error

--- test/unit/ConfigurationTest.rb
+++ test/unit/ConfigurationTest.rb
@@ -31,8 +31,9 @@ class ConfigurationTest < Test::Unit::TestCase
   end
 
   def test_invalid_proxy_uri
+    proxy_uri = 'not_://*a_valid_proxy'
     assert_raise Trolley::Configuration::InvalidProxyAddress.new("Invalid proxy provided to configuration: #{proxy_uri}") do
-      Trolley::Configuration.new('k', 's', proxy_uri: 'not_://*a_valid_proxy').proxy
+      Trolley::Configuration.new('k', 's', proxy_uri:).proxy
     end
   end
 
--- test/unit/TrolleyTest.rb
+++ test/unit/TrolleyTest.rb
@@ -7,8 +7,9 @@ class TrolleyTest < Test::Unit::TestCase
   end
 
   def test_client_invalid_proxy_uri
+    proxy_uri = 'not_://*a_valid_proxy'
     assert_raise Trolley::Configuration::InvalidProxyAddress.new("Invalid proxy provided to configuration: #{proxy_uri}") do
-      Trolley.client('k', 's', proxy_uri: 'not_://*a_valid_proxy')
+      Trolley.client('k', 's', proxy_uri:)
     end
   end
 end
